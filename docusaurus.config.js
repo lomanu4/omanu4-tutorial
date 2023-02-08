8fd3b2a67149bace25ad7faf8b29d797b5bba851
lomanu4 <mapumah@inbox.ru>
2023-02-09 00:40:02 +0100

    fdfd

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'lomanu4', // Usually your GitHub org/user name.
-  projectName: 'Lomanu4-tutorial', // Usually your repo name.
+  projectName: 'omanu4-tutorial', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',

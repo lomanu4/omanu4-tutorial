b550ed80b36d970a01d86da3ba32768fea618863
lomanu4 <mapumah@inbox.ru>
2023-02-09 12:49:18 +0100

    dsds

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,12 +4,11 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Forum',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Зайди на наш форум GuardianeLinks.com где вы сможете отблагодарить или помочь для потдежки проекта.
       </>
     ),
   },
